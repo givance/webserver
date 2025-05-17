c96af1b6aab290bf278a932892447267420372ae
Yulong Liu <lyl890808@gmail.com>
2025-05-16 22:10:57 -0700

    .

--- drizzle/migrations/0007_yellow_the_hunter.sql
+++ drizzle/migrations/0007_yellow_the_hunter.sql
@@ -1,2 +1,5 @@
-ALTER TABLE "organizations" ALTER COLUMN "memory" SET DEFAULT '{}';--> statement-breakpoint
+UPDATE "organizations" SET "memory" = '{}'::text[] WHERE "memory" IS NULL;
+--> statement-breakpoint
+ALTER TABLE "organizations" ALTER COLUMN "memory" SET DEFAULT '{}'::text[];
+--> statement-breakpoint
 ALTER TABLE "organizations" ALTER COLUMN "memory" SET NOT NULL;
