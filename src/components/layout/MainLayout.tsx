f43f20f96d012a4f1a31d5acbb88f43e2f1333d6
Yulong Liu <lyl890808@gmail.com>
2025-06-08 20:58:57 -0700

    make sidebar fixed.

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -112,9 +112,9 @@ export default function MainLayout({
   return (
     <html lang="en">
       <body className={cn(geistSans.variable, geistMono.variable, "antialiased bg-gray-50")}>
-        <div className="min-h-screen w-full flex">
+        <div className="min-h-screen w-full">
           <SidebarProvider>
-            <Sidebar className="w-64 border-r bg-white flex flex-col">
+            <Sidebar className="w-64 border-r bg-white flex flex-col fixed left-0 top-0 h-screen z-50">
               <SidebarHeader className="flex-none">
                 <Link
                   href="/"
@@ -124,7 +124,7 @@ export default function MainLayout({
                   <span className="font-semibold text-lg">Givance</span>
                 </Link>
               </SidebarHeader>
-              <SidebarContent className="flex-1">
+              <SidebarContent className="flex-1 overflow-y-auto">
                 <div className="px-3 py-2 space-y-0">
                   {/* Donor Management - Collapsible */}
                   <Collapsible open={isDonorOpen} onOpenChange={setIsDonorOpen}>
@@ -274,7 +274,7 @@ export default function MainLayout({
               </SidebarContent>
               <SidebarFooter className="flex-none" />
             </Sidebar>
-            <div className="flex-1 flex flex-col">
+            <div className="flex-1 flex flex-col ml-64">
               <Header />
               <main className="flex-1">
                 <TRPCProvider>{children}</TRPCProvider>
